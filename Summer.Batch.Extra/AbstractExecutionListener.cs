fba886f967f0525f840f07ee57831137d5f11b52
erik75269 <erik75269@gmail.com>
2020-04-10 18:41:35 -0500

    Fixed PostProcesser to support restartability.

--- Summer.Batch.Extra/AbstractExecutionListener.cs
+++ Summer.Batch.Extra/AbstractExecutionListener.cs
@@ -15,6 +15,7 @@
 using System;
 using Microsoft.Practices.Unity;
 using NLog;
+using Summer.Batch.Common.Transaction;
 using Summer.Batch.Core;
 
 
@@ -83,6 +84,9 @@ namespace Summer.Batch.Extra
             ExitStatus returnStatus = stepExecution.ExitStatus;
             if (!"FAILED".Equals(returnStatus.ExitCode))
             {
+                using (var scope = TransactionScopeManager.CreateScope())
+                {
+                    {
                         try
                         {
                             returnStatus = Postprocess();
@@ -92,7 +96,11 @@ namespace Summer.Batch.Extra
                             // Need to catch exception to log and set status to FAILED, while
                             // Spring batch would only log and keep the status COMPLETED
                             Logger.Error(e, "Exception during postprocessor");
-                            returnStatus = ExitStatus.Failed;
+                            stepExecution.UpgradeStatus(BatchStatus.Failed);
+                            throw;
+                        }
+                        scope.Complete();
+                    }
                 }
             }
             return returnStatus;
